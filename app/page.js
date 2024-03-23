77c76e807866d45a3018ac0b2fe4a7f11838a592
p4nthera115 <asdshuja@hotmail.co.uk>
2024-03-23 22:02:51 +0100

    height fix

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import UnderConstruction from "@/components/UnderConstruction";
 
 export default function Home() {
   return (
-    <div className="bg-neutral-950 h-screen w-screen flex">
+    <div className="bg-neutral-950 h-full w-full flex">
       <UnderConstruction />
     </div>
   );
