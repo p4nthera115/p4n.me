9a8b6ae64ade913cc8dbb8849eb178139e20078c
p4nthera115 <asdshuja@hotmail.co.uk>
2024-03-24 05:21:26 +0100

    animation start

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,28 +1,45 @@
 "use client";
 
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function ProjectCard({ title, url, image, description }) {
   const [active, setActive] = useState(false);
 
   const handleClick = () => {
     setActive(!active);
-
-    console.log(active, title);
   };
+
+  const inactiveStyle = "";
+  const activeStyle = "";
+
   return (
-    <div
-      className="w-full h-[11rem] p-6 flex relative flex-row gap-10 rounded-2xl bg-[#181818] cursor-pointer hover:-translate-y-1 hover:shadow-md shadow-black transition"
+    <motion.div
+      transition={{
+        ease: "easeInOut",
+        duration: 0.2,
+      }}
+      animate={
+        active
+          ? {
+              width: "100%",
+              height: "100%",
+              zIndex: 100,
+              position: "absolute",
+            }
+          : { marginX: "auto" }
+      }
+      className="w-[90%] sm:w-3/4 md:w-1/2 mx-auto h-[11rem] p-6 flex relative flex-row gap-10 rounded-2xl bg-[#181818] cursor-pointer hover:-translate-y-1 hover:shadow-md shadow-black transition"
       onClick={handleClick}
     >
-      <div className="flex h-full w-full aspect-video bg-white rounded-lg"></div>
+      {/* <div className="flex h-full w-full aspect-video bg-white rounded-lg"></div>
       <div className="flex flex-col gap-4">
         <h2 className="text-2xl">{title}</h2>
         <p className="text-xs md:text-sm">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
           possimus consequuntur, distinctio, doloribus autem in veritatis ad.
         </p>
-      </div>
-    </div>
+      </div> */}
+    </motion.div>
   );
 }
