33108d2fbb068f231b6f2f7c5e4ce29f569f9a99
p4nthera115 <asdshuja@hotmail.co.uk>
2024-04-15 17:34:24 +0200

    cleanup

--- components/Stack.js
+++ components/Stack.js
@@ -13,47 +13,6 @@ import {
 import { motion } from "framer-motion";
 
 export default function Stack({ active, stack, setHover }) {
-  const stackArr = [
-    {
-      icon: (
-        <SiTypescript className="hover:scale-105 transition hover:text-blue-500" />
-      ),
-      name: "Typescript",
-    },
-    {
-      icon: <SiNextdotjs className="hover:scale-105 transition" />,
-      name: "Next.js",
-    },
-    {
-      icon: (
-        <SiTailwindcss className="hover:scale-105 transition hover:text-sky-400" />
-      ),
-      name: "TailwindCSS",
-    },
-    {
-      icon: <SiThreedotjs className="hover:scale-105 transition" />,
-      name: "Three.js",
-    },
-    {
-      icon: (
-        <SiSupabase className="hover:scale-105 transition hover:text-green-400" />
-      ),
-      name: "Supabase",
-    },
-    {
-      icon: (
-        <SiCloudflare className="hover:scale-105 transition hover:text-orange-400" />
-      ),
-      name: "Cloudflare",
-    },
-    {
-      icon: (
-        <SiPrisma className="hover:scale-105 transition hover:text-sky-900" />
-      ),
-      name: "Prisma",
-    },
-  ];
-
   return (
     <motion.ol className="flex flex-row gap-3 text-4xl p-1 select-none">
       {stack.map((item, i) => (
