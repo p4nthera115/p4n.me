40a2d7f96be80dbb3996fdb7ed6e70fafdd68abb
p4nthera115 <asdshuja@hotmail.co.uk>
2024-05-04 16:13:29 +0200

    socials

--- components/MobileSocials.js
+++ components/MobileSocials.js
@@ -3,14 +3,22 @@ import { FaGithub, FaInstagram, FaSpotify, FaXTwitter } from "react-icons/fa6";
 
 export default function MobileSocials() {
   return (
-    <motion.div className="px-4 rounded-lg bg-[#181818] flex flex-row gap-4 justify-between ">
-      <div className="self-center items-center">
-        <FaSpotify className="h-12 w-6 flex" />
-      </div>
-      <div className="flex flex-row gap-3">
-        <FaXTwitter className="h-12 w-6 flex" />
-        <FaGithub className="h-12 w-6 flex" />
-        <FaInstagram className="h-12 w-6 flex" />
+    <motion.div className="px-4 rounded-lg bg-[#181818] flex flex-row  justify-center self-center ">
+      {/* <div className="self-center items-center"> */}
+      {/* </div> */}
+      <div className="flex flex-row gap-5">
+        <a href="https://www.x.com/p4nthera_" target="_blank">
+          <FaXTwitter className="h-12 w-6 flex" />
+        </a>
+        <a href="https://www.github.com/p4nthera115" target="_blank">
+          <FaGithub className="h-12 w-6 flex" />
+        </a>
+        <a href="https://www.instagram.com/_p4nthera" target="_blank">
+          <FaInstagram className="h-12 w-6 flex" />
+        </a>
+        <a href="https://open.spotify.com/user/asdshuja06" target="_blank">
+          <FaSpotify className="h-12 w-6 flex" />
+        </a>
       </div>
     </motion.div>
   );
