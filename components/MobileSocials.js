cf2bc02f659d82ee98ae48fdffbab43fca8e7276
p4nthera115 <asdshuja@hotmail.co.uk>
2024-05-04 16:35:54 +0200

    height fix

--- components/MobileSocials.js
+++ components/MobileSocials.js
@@ -3,7 +3,18 @@ import { FaGithub, FaInstagram, FaSpotify, FaXTwitter } from "react-icons/fa6";
 
 export default function MobileSocials() {
   return (
-    <motion.div className="px-4 rounded-lg bg-[#181818] flex flex-row  justify-center self-center ">
+    <motion.div
+      initial={{ opacity: 0, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 1.0,
+        type: "tween",
+        ease: "easeInOut",
+        staggerChildren: 0.2,
+        staggerDirection: 1,
+      }}
+      className="px-4 rounded-lg bg-[#181818] flex flex-row  justify-center self-center "
+    >
       {/* <div className="self-center items-center"> */}
       {/* </div> */}
       <div className="flex flex-row gap-5">
