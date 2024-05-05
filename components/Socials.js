12fd51a9347870d5ee0b9bddf4d298e67d72767d
p4nthera115 <asdshuja@hotmail.co.uk>
2024-05-05 16:19:41 +0200

    hover socials

--- components/Socials.js
+++ components/Socials.js
@@ -6,16 +6,16 @@ export default function MobileSocials() {
     <div className="p-4 m-8 rounded-lg bg-[#181818] absolute right-0 top-1/2 -translate-y-1/2">
       <div className="flex flex-col gap-5 items-center self-center text-3xl">
         <a href="https://www.x.com/p4nthera_" target="_blank">
-          <FaXTwitter />
+          <FaXTwitter className="hover:scale-110 transition" />
         </a>
         <a href="https://www.github.com/p4nthera115" target="_blank">
-          <FaGithub />
+          <FaGithub className="hover:scale-110 transition" />
         </a>
         <a href="https://www.instagram.com/_p4nthera" target="_blank">
-          <FaInstagram />
+          <FaInstagram className="hover:scale-110 transition" />
         </a>
         <a href="https://open.spotify.com/user/asdshuja06" target="_blank">
-          <FaSpotify />
+          <FaSpotify className="hover:scale-110 transition" />
         </a>
       </div>
     </div>
